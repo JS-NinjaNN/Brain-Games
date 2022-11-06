3dd88dfe6e93a747ca73bcf2e7acd5a210800530
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-06 13:32:42 +0300

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const playerWelcome = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}`)
-}
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+};
