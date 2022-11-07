799c8547511b83a10051e94f1ba45d59b9f0cdf0
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-08 00:15:11 +0300

    update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,47 +1,36 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-
-const getRandomNumber = () => {
-  const min = Math.ceil(1);
-  const max = Math.floor(100);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+import { getRandomNumber, playerWelcome, playerQuestion } from '../src/index.js';
 
 const isEven = (num) => {
-  const result = num % 2 === 0;
-  return result;
-};
-
-const playerWelcome = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
-  return name;
+  const even = (num % 2 === 0) ? 'yes' : 'no';
+  return even;
 };
 
 const brainEven = () => {
   let correctAnswers = 0;
+  let playerLose = '';
+  const rules = 'Answer "yes" if the number is even, otherwise answer "no"';
   const name = playerWelcome();
-  console.log('Answer "yes" if the number is even, otherwise answer "no"');
+  const playerWin = `Congratulations, ${name}!`;
+  console.log(rules);
   while (correctAnswers < 3) {
     const randomNumber = getRandomNumber();
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer !== 'yes' && answer !== 'no') {
-      break;
-    } else if ((isEven(randomNumber)) && (answer === 'yes')) {
-      correctAnswers += 1;
+    console.log(randomNumber);
+    const correctAnswer = isEven(randomNumber);
+    const playerAnswer = playerQuestion();
+    if (correctAnswer === playerAnswer) {
       console.log('Correct!');
-    } else if ((!isEven(randomNumber)) && answer === 'no') {
       correctAnswers += 1;
-      console.log('Correct!');
     } else {
+      playerLose = `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`;
       break;
     }
   }
-  if (correctAnswers === 3) {
-    return console.log(`Congratulations, ${name}!`);
+  if (correctAnswers >= 3) {
+    console.log(playerWin);
+  } else {
+    console.log(playerLose);
   }
-  return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
 };
+
 brainEven();
