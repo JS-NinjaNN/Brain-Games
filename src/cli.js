bc987ade2d57113f4a598a95d6483b7427c6892d
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-08 00:15:21 +0300

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,4 +4,5 @@ export default () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
+  return name;
 };
