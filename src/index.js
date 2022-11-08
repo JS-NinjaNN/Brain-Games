24104e871d6ccc48f298006be4cc1144473e1719
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-08 23:15:55 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const getRandomNumber = () => {
   const min = Math.ceil(1);
-  const max = Math.floor(25);
+  const max = Math.floor(100);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
