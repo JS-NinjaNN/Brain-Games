7a9ecc468c6b7e96fd45617c5d95bdfc013fb006
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-08 22:08:57 +0300

    update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,36 +1,35 @@
 #!/usr/bin/env node
-import { getRandomNumber, playerWelcome, playerQuestion } from '../src/index.js';
+import * as fun from '../src/index.js';
+import playerWelcome from '../src/cli.js';
 
 const isEven = (num) => {
   const even = (num % 2 === 0) ? 'yes' : 'no';
   return even;
 };
 
+const rules = 'Answer "yes" if the number is even, otherwise answer "no"';
+let correctAnswers = 0;
+let name;
+let randomNumber;
+let correctAnswer;
+let playerAnswer;
+
 const brainEven = () => {
-  let correctAnswers = 0;
-  let playerLose = '';
-  const rules = 'Answer "yes" if the number is even, otherwise answer "no"';
-  const name = playerWelcome();
-  const playerWin = `Congratulations, ${name}!`;
+  name = playerWelcome();
   console.log(rules);
   while (correctAnswers < 3) {
-    const randomNumber = getRandomNumber();
+    randomNumber = fun.getRandomNumber();
     console.log(randomNumber);
-    const correctAnswer = isEven(randomNumber);
-    const playerAnswer = playerQuestion();
+    correctAnswer = isEven(randomNumber);
+    playerAnswer = fun.playerQuestion();
     if (correctAnswer === playerAnswer) {
       console.log('Correct!');
       correctAnswers += 1;
     } else {
-      playerLose = `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`;
       break;
     }
   }
-  if (correctAnswers >= 3) {
-    console.log(playerWin);
-  } else {
-    console.log(playerLose);
-  }
+  fun.gameOver(correctAnswers, playerAnswer, correctAnswer, name);
 };
 
 brainEven();
