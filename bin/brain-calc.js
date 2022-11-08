e1158360fd8521de91c7c5a22422c879f28a894a
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-08 20:07:10 +0300

    update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,53 +1,30 @@
 #!/usr/bin/env node
-import {
-  getRandomNumber, getRandomOperator, playerWelcome, playerQuestion,
-} from '../src/index.js';
+import * as fun from '../src/index.js';
+import playerWelcome from '../src/cli.js';
 
-const operation = (a, operator, b) => {
-  const example = [a, operator, b];
-  return example;
-};
-
-const getCorrectAnswer = (example) => {
-  let correctAnswer;
-  switch (example[1]) {
-    case '+':
-      correctAnswer = example[0] + example[2];
-      break;
-    case '-':
-      correctAnswer = example[0] - example[2];
-      break;
-    default:
-      correctAnswer = example[0] * example[2];
-  }
-  return correctAnswer;
-};
+const rules = 'What is the result of the expression?';
+let correctAnswer;
+let playerAnswer;
+let example;
+let correctAnswers = 0;
+let name;
 
 const brainCalc = () => {
-  let correctAnswers = 0;
-  let playerLose = '';
-  const rules = 'What is the result of the expression?';
-  const name = playerWelcome();
-  const playerWin = `Congratulations, ${name}!`;
+  name = playerWelcome();
   console.log(rules);
   while (correctAnswers < 3) {
-    const example = operation(getRandomNumber(), getRandomOperator(), getRandomNumber());
-    const correctAnswer = getCorrectAnswer(example);
+    example = fun.operation(fun.getRandomNumber(), fun.getRandomOperator(), fun.getRandomNumber());
+    correctAnswer = fun.getCorrectAnswer(example);
     console.log(`Question: ${example[0]}${example[1]}${example[2]}`);
-    const playerAnswer = playerQuestion();
+    playerAnswer = fun.playerQuestion();
     if (+correctAnswer === +playerAnswer) {
       console.log('Correct!');
       correctAnswers += 1;
     } else {
-      playerLose = `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`;
       break;
     }
   }
-  if (correctAnswers >= 3) {
-    console.log(playerWin);
-  } else {
-    console.log(playerLose);
-  }
+  console.log(fun.gameOver(correctAnswers, playerAnswer, correctAnswer, name));
 };
 
 brainCalc();
