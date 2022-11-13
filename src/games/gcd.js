74233a492d28b40304028d258aa533b176eb3d09
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-14 00:21:26 +0300

    add getNewRound fun

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,4 +1,5 @@
 import * as fun from '../index.js';
+import getRandomNumber from '../utils.js';
 
 const getGCD = (a, b) => {
   let answer;
@@ -21,15 +22,19 @@ let givenNumberTwo;
 let correctAnswers = 0;
 let name;
 
+const getNewRound = () => {
+  givenNumberOne = getRandomNumber(1, 50);
+  givenNumberTwo = getRandomNumber(1, 50);
+  console.log(`Question: ${givenNumberOne} ${givenNumberTwo}`);
+  correctAnswer = getGCD(givenNumberOne, givenNumberTwo);
+  playerAnswer = fun.playerQuestion();
+};
+
 const brainGcd = () => {
   name = fun.playerGreetings();
   console.log(rules);
   while (correctAnswers < 3) {
-    givenNumberOne = fun.getRandomNumber(1, 50);
-    givenNumberTwo = fun.getRandomNumber(1, 50);
-    console.log(`Question: ${givenNumberOne} ${givenNumberTwo}`);
-    correctAnswer = getGCD(givenNumberOne, givenNumberTwo);
-    playerAnswer = fun.playerQuestion();
+    getNewRound();
     if (+correctAnswer === +playerAnswer) {
       console.log('Correct!');
       correctAnswers += 1;
