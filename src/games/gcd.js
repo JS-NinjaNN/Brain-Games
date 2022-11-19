e595685a6f13c257634de693847e6b71fe472356
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-19 14:44:06 +0300

    update getNewRounds() and brainGCD()

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,5 +1,7 @@
-import * as fun from '../index.js';
+import run from '../index.js';
 import getRandomNumber from '../utils.js';
+
+const rules = 'Find the greatest common divisor of given numbers.';
 
 const getGCD = (a, b) => {
   let answer;
@@ -14,35 +16,22 @@ const getGCD = (a, b) => {
   return answer;
 };
 
-const rules = 'Find the greatest common divisor of given numbers.';
-let correctAnswer;
-let playerAnswer;
-let givenNumberOne;
-let givenNumberTwo;
-let correctAnswers = 0;
-let name;
-
-const getNewRound = () => {
-  givenNumberOne = getRandomNumber(1, 50);
-  givenNumberTwo = getRandomNumber(1, 50);
-  console.log(`Question: ${givenNumberOne} ${givenNumberTwo}`);
-  correctAnswer = getGCD(givenNumberOne, givenNumberTwo);
-  playerAnswer = fun.playerQuestion();
+const getNewRounds = () => {
+  // [0] - вопрос, [1] - ответ;
+  const rounds = [];
+  for (let i = 0; i < 3; i += 1) {
+    const items = [];
+    const givenNumberOne = getRandomNumber(1, 50);
+    const givenNumberTwo = getRandomNumber(1, 50);
+    items.push(`Question: ${givenNumberOne} ${givenNumberTwo}`);
+    items.push(String(getGCD(givenNumberOne, givenNumberTwo)));
+    rounds.push(items);
+  }
+  return rounds;
 };
 
 const brainGcd = () => {
-  name = fun.playerGreetings();
-  console.log(rules);
-  while (correctAnswers < 3) {
-    getNewRound();
-    if (+correctAnswer === +playerAnswer) {
-      console.log('Correct!');
-      correctAnswers += 1;
-    } else {
-      break;
-    }
-  }
-  fun.gameOver(correctAnswers, playerAnswer, correctAnswer, name);
+  run(rules, getNewRounds());
 };
 
 export default brainGcd;
