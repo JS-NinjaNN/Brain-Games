a44f9d212c88766f8fc238b598bdf53343a1221b
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-22 22:36:19 +0300

    update run()

--- src/index.js
+++ src/index.js
@@ -5,47 +5,25 @@ const playerQuestion = () => {
   return result;
 };
 
-const playerLose = (incorrect, correct, player) => `'${incorrect}' is wrong answer ;(. Correct answer was '${correct}'.\nLet's try again, ${player}!`;
-const playerWin = (player) => `Congratulations, ${player}!`;
-
-const gameOver = (correctAnswers, incorrect, correct, player) => {
-  if (correctAnswers > 2) {
-    console.log(playerWin(player));
-  } else {
-    console.log(playerLose(incorrect, correct, player));
-  }
-};
-
-const playerGreetings = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
-  return name;
-};
-
 const run = (rules, rounds) => {
-  let correctAnswers = 0;
   let playerAnswer;
   let correctAnswer;
-  let roundsCount = 0;
-  const username = playerGreetings();
+  console.log('Welcome to the Brain Games!');
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}`);
   console.log(rules);
-  while (correctAnswers < 3) {
-    // [0] - вопрос, [1] - правильный ответ;
-    const currentRound = rounds[roundsCount];
+  for (let i = 0; i < 3; i += 1) {
+    const currentRound = rounds[i];
     const [question, answer] = currentRound;
     console.log(question);
-    playerAnswer = playerQuestion();
     correctAnswer = answer;
-    if (correctAnswer === playerAnswer) {
-      console.log('Correct!');
-      correctAnswers += 1;
-    } else {
-      return gameOver(correctAnswers, playerAnswer, answer, username);
+    playerAnswer = playerQuestion();
+    if (correctAnswer !== playerAnswer) {
+      return console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${username}!`);
     }
-    roundsCount += 1;
+    console.log('Correct!');
   }
-  return gameOver(correctAnswers, playerAnswer, correctAnswer, username);
+  return console.log(`Congratulations, ${username}!`);
 };
 
 export default run;
