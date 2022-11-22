effc02854f5fdee00ca07eddfa9f57ad926675ce
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-22 20:53:00 +0300

    rename import to run

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,4 +1,4 @@
-import runPrime from '../index.js';
+import run from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
@@ -24,7 +24,7 @@ const getNewRound = () => {
 };
 
 const brainPrime = () => {
-  runPrime(rules, getNewRound());
+  run(rules, getNewRound());
 };
 
 export default brainPrime;
