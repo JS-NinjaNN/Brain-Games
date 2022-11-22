315373a8b380963f509a7968ce93ad0a0470a264
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-22 22:48:31 +0300

    update runProgression()

--- src/games/progression.js
+++ src/games/progression.js
@@ -29,7 +29,11 @@ const getNewRound = () => {
 };
 
 const runProgression = () => {
-  run(rules, getNewRound());
+  const rounds = [];
+  for (let i = 0; i < 3; i += 1) {
+    rounds.push(getNewRound());
+  }
+  run(rules, rounds);
 };
 
 export default runProgression;
