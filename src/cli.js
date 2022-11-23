021096e03af9f67040d0f34ac48650edaeec8eca
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-23 21:01:44 +0300

    rename greet()

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+const greet = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
 };
+
+export default greet;
