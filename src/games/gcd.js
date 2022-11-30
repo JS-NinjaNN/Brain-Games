b063e8164b233c3c44705d78c04f10d0616c6faa
ToxicNN <moiseenko.artem94@yandex.ru>
2022-11-30 22:03:50 +0300

    update getGCD()

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,18 +5,7 @@ const description = 'Find the greatest common divisor of given numbers.';
 const min = 1;
 const max = 50;
 
-const getGCD = (x, y) => {
-  let answer;
-  let i = x > y ? x : y;
-  while (i > 0) {
-    if (x % i === 0 && y % i === 0) {
-      answer = i;
-      break;
-    }
-    i -= 1;
-  }
-  return answer;
-};
+const getGCD = (x, y) => (x % y ? getGCD(y, x % y) : y);
 
 const getNewRound = () => {
   const number1 = getRandomNumber(min, max);
